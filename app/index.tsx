30c5defc1dd07a939aa83a164c9f24b8f40992bb
BrenoFreitasM <brenofreitasmendes@gmail.com>
2025-05-10 17:17:26 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+
+const index = () => {
+    return (
+        <View>
+            <Text>index</Text>
+        </View>
+    )
+}
