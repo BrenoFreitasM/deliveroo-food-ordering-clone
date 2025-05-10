36baa9ab92ca6a6b443cff525f7f897a59606043
BrenoFreitasM <brenofreitasmendes@gmail.com>
2025-05-10 20:27:18 -0300

    feat: search bar

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -1,5 +1,6 @@
 import Colors from "@/constants/Colors";
 import { Ionicons } from "@expo/vector-icons";
+import { Link } from "expo-router";
 import React from "react";
 import {
   Image,
@@ -7,9 +8,34 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   View,
 } from "react-native";
+
+const SearchBar = () => (
+  <View style={styles.searchContainer}>
+    <View style={styles.searchSection}>
+      <View style={styles.searchField}>
+        <Ionicons
+          style={styles.SearchIcon}
+          name="ios-search"
+          size={20}
+          color={Colors.medium}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Restaurants, groceries, dishes"
+        />
+      </View>
+      <Link href={"/"} asChild>
+        <TouchableOpacity style={styles.optionButton}>
+          <Ionicons name="options-outline" size={20} color={Colors.primary} />
+        </TouchableOpacity>
+      </Link>
+    </View>
+  </View>
+);
 
 const CustomHeader = () => {
   return (
@@ -34,6 +60,7 @@ const CustomHeader = () => {
           <Ionicons name="person-outline" size={20} color={Colors.primary} />
         </TouchableOpacity>
       </View>
+      <SearchBar />
     </View>
   );
 };
@@ -76,6 +103,36 @@ const styles = StyleSheet.create({
   locationName: { flexDirection: "row", alignItems: "center" },
   profileButton: {
     backgroundColor: Colors.lightGrey,
+    padding: 10,
+    borderRadius: 50,
+  },
+  searchContainer: {
+    height: 60,
+    backgroundColor: "#fff",
+  },
+
+  searchSection: {
+    flexDirection: "row",
+    gap: 10,
+    flex: 1,
+    paddingHorizontal: 20,
+    alignItems: "center",
+  },
+  searchField: {
+    flex: 1,
+    backgroundColor: Colors.lightGrey,
+    borderRadius: 8,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input: {
+    padding: 10,
+    color: Colors.mediumDark,
+  },
+  SearchIcon: {
+    paddingLeft: 10
+  },
+  optionButton: {
     padding: 10,
     borderRadius: 50,
   },
